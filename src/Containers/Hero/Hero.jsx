d21924bf387fca3b9b70adb475bda7a294923a7e
Leo W <leo.westebbe@gmail.com>
2022-12-02 16:50:47 -0500

    Picture replacements

--- src/Containers/Hero/Hero.jsx
+++ src/Containers/Hero/Hero.jsx
@@ -1,22 +1,42 @@
 import React from "react";
 import './Hero.css'
-import html5 from './assets/html5.svg'
-import css3 from './assets/css3.svg'
-import js from './assets/js.svg'
-import vite from './assets/vite.svg'
-import react from './assets/react.svg'
-import jest from "./assets/jest.svg";
-import git from "./assets/git.svg";
-import github from "./assets/github.svg";
-import mocha from "./assets/mocha.svg";
+import HTML from './assets/HTML.svg'
+import CSS from './assets/CSS.svg'
+import JavaScript from './assets/JavaScript.svg'
+import Vite from './assets/Vite.svg'
+import Reactjs from './assets/Reactjs.svg'
+import Jest from "./assets/Jest.svg";
+import Git from "./assets/Git.svg";
+import GitHub from "./assets/GitHub.svg";
+import Mocha from "./assets/Mocha.svg";
 import npm from "./assets/npm.svg";
 
 export const Hero = () => {
+    const handleOnMouseOver = e => {
+        const className = e.target.className
+        document.querySelector(`p.logo.${className}`).style.visibility="visible"
+    }
+    const handleOnMouseLeave = e => {
+        const className = e.target.className
+        document.querySelector(`p.logo.${className}`).style.visibility="hidden"
+    }
     const technologies = () => {
-        const technologiesArray = [html5, css3, js, react, vite, jest, mocha, git, github, npm]
+        const technologiesArray = [HTML, CSS, JavaScript, Reactjs, Vite, Jest, Mocha, Git, GitHub, npm]
+        const regex = /(?<=assets\/)\w*/
         return (
             technologiesArray.map(tech => {
-                return <img src={tech} className={`logo`}></img>
+                const techName = tech.match(regex)[0]
+                return (
+                <div className={`logoContainer ${techName}`} key={techName}>
+                    <img src={tech} 
+                    onMouseOver={handleOnMouseOver} 
+                    onMouseLeave={handleOnMouseLeave} 
+                    className={`${techName}`} 
+                    key={techName} 
+                    alt={`${techName} logo`}/>
+                    <p className={`logo ${techName}`} style={{visibility: 'hidden'}} >{techName}</p>
+                </div>
+                )
             })
         )
     }
